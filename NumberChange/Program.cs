9bedb6971ff9b679edb8f2b786dcf43203405e96
Cevher YILMAZ <38843825+cevheryilmaz@users.noreply.github.com>
2019-05-07 13:19:14 +0300

    Add files via upload

---
+++ NumberChange/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberChange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 5;
+            int number2 = 10;
+
+            Console.WriteLine("Previous Value");
+            Console.WriteLine("Number 1={0}", number1);
+            Console.WriteLine("Number 2={0}", number2);
+
+
+            number1 = number1 + number2;
+            number2 = number1 - number2;
+            number1 = number1 - number2;
+            Console.WriteLine("_______________________________________");
+            Console.WriteLine("Next Value");
+            Console.WriteLine("Number 1={0}", number1);
+            Console.WriteLine("Number 2={0}", number2);
+
+            Console.ReadKey();
+        }
+    }
+}
